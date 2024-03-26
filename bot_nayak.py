952c23a430a6fff58f060e9b6aa9e8833e33dd9c
VashitvaR <vashitva.bagga@gmail.com>
2024-03-27 00:46:52 +0530

    cc

--- bot_nayak.py
+++ bot_nayak.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pdfplumber 
+from gensim.summarization import summarize
 
 def extract_text(feed):
     text = ""
@@ -8,13 +9,18 @@ def extract_text(feed):
             text += page.extract_text()
     return text
 
-st.title("PDF Text Extractor")
+st.title("PDF Text Extractor and Summarizer")
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 
 if uploaded_file is not None:
     extracted_text = extract_text(uploaded_file)
     if extracted_text:
+        st.subheader("Extracted Text:")
         st.text(extracted_text)
+        
+        st.subheader("Summarized Text:")
+        summarized_text = summarize(extracted_text, ratio=0.25)
+        st.text(summarized_text)
     else:
         st.write("No text found in the PDF file.")
