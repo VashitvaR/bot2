68cb955977ce7958ca31947f995b798fae5e3428
VashitvaR <vashitva.bagga@gmail.com>
2024-03-27 00:55:03 +0530

    cc

--- bot_nayak.py
+++ bot_nayak.py
@@ -15,12 +15,10 @@ uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 
 if uploaded_file is not None:
     extracted_text = extract_text(uploaded_file)
-    if extracted_text:
-        st.subheader("Extracted Text:")
-        st.text(extracted_text)
+   
         
-        st.subheader("Summarized Text:")
-        summarized_text = summarize(extracted_text, ratio=0.25)
-        st.text(summarized_text)
-    else:
+    st.subheader("Summarized Text:")
+    summarized_text = summarize(extracted_text, ratio=0.25)
+    st.text(summarized_text)
+else:
         st.write("No text found in the PDF file.")
